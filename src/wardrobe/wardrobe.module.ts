077594c48d43db9af40d4ac0f9f1d0e640d08027
Daniyal Qamar <daniyalqamar1007@gmail.com>
2025-05-09 21:38:34 +0500

    add file acceptenace on body of post wardrobe-item api and remove image url

--- src/wardrobe/wardrobe.module.ts
+++ src/wardrobe/wardrobe.module.ts
@@ -3,15 +3,21 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { WardrobeService } from './wardrobe.service';
 import { WardrobeController } from './wardrobe.controller';
 import { WardrobeItem, WardrobeItemSchema } from './schemas/wardrobe.schema';
+import { AwsService } from '../aws/aws.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     MongooseModule.forFeature([
       { name: WardrobeItem.name, schema: WardrobeItemSchema },
     ]),
   ],
   controllers: [WardrobeController],
-  providers: [WardrobeService],
+  providers: [WardrobeService, AwsService],
   exports: [WardrobeService],
 })
 export class WardrobeModule {}
