0fa0168f5e997132852340960a08af5cfd159419
Daniyal Qamar <daniyalqamar1007@gmail.com>
2025-12-05 23:40:37 +0500

    install fixes

--- src/main.ts
+++ src/main.ts
@@ -31,6 +31,7 @@ async function bootstrap() {
     credentials: true, // allow cookies and authorization headers
   });
 
-  await app.listen(3099);
+  await app.listen(3099, '0.0.0.0');
+  console.log(`Application is running on: http://0.0.0.0:3099`);
 }
 bootstrap();
